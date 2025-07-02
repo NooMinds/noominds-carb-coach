4761b1502176f5b5e1338f591ed3361fbe0a8851
NooMinds <sales@noominds.com>
2025-07-02 10:58:05 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+/**
+ * NooMinds Carb Coach - Main Entry Point
+ * 
+ * This is the main entry point for the NooMinds Carb Coach application.
+ * It sets up React Router and renders the App component.
+ * 
+ * The app is designed to help endurance athletes train their gut to tolerate
+ * higher carbohydrate intake rates during training and competition.
+ */
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+);
